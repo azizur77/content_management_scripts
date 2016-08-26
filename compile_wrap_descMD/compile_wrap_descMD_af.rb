31267f6f4779412e95befd5a77ffd7dc26a6c285
Arcadia Falcone <arcadia@stanford.edu>
2016-08-26 15:26:08 -0700

    Add folder for MODS compile scripts

---
+++ compile_wrap_descMD/compile_wrap_descMD_af.rb
@@ -0,0 +1,85 @@
+require 'rubygems'
+require 'nokogiri'
+
+# this script compiles XML descriptive metadata records coming from druid-named files into one file
+# the required wrappers for Argo's MODS bulk upload are added
+
+# druid-named files are defined as:
+# [druid].xml, e.g. bb020sf5359.xml
+# druid:[druid], where : may be any character, e.g. druid:bb020sf5359
+# druid_[druid].xml, where _ may be any character, e.g. e.g. druid_bb020sf5359.xml
+
+# example wrapper
+
+# <xmlDocs xmlns="http://library.stanford.edu/xmlDocs" datetime="YYYY-MM-DD HH:MM:SSAM" sourceFile="filename.xml">
+#  <xmlDoc id="descMetadata" objectId="#{druid}">
+#    <mods>
+#        ...
+#    </mods>
+#  </xmlDoc>
+# </xmlDocs>
+
+# to run the script, it needs two input parameters:
+# 1. the folder of files to process
+# 2. the filename for the new file
+
+# the new file is written to the folder of files to process by default, unless you specify another location (a relative path) from this pwd
+
+# command to run the script:
+# ruby compile_wrap_descMD.rb <path to folder of files to process> <new filename>
+
+folder = ARGV[0]
+descMD = ARGV[1]
+
+time = Time.now.strftime('%Y-%m-%d %I:%M:%S%p')
+
+# change working directory to folder for processing files
+Dir.chdir(folder) do
+  # creates a new file for the compiled records
+  newFile = File.new(descMD.to_s, 'w')
+  newFile.write("<xmlDocs xmlns=\"http://library.stanford.edu/xmlDocs\" datetime=\"#{time}\" sourceFile=\"#{descMD}\">")
+
+  # read the filenames in directory into an array for looking up
+  filenames = Dir.entries('.') #=> ["bc849vk0855.xml", "bc994zz9154.xml"]
+
+  # iterate through filenames
+  filenames.each do |file|
+    if file =~ /^[a-z]{2}[0-9]{3}[a-z]{2}[0-9]{4}\.xml/
+      # get druid from filename
+      druid = file.gsub(/\.xml/, '')
+
+      # open druid-named files
+      doc = Nokogiri::XML(open(file.to_s))
+      record = doc.root
+
+      # add xmlDoc wrapper
+      newFile.write("<xmlDoc id=\"descMetadata\" objectId=\"#{druid}\">" + record.to_xml + '</xmlDoc>')
+
+    elsif file =~ /druid.[a-z]{2}[0-9]{3}[a-z]{2}[0-9]{4}\.xml/
+      # get druid from filename
+      temp = file.gsub(/druid./, '')
+      druid = temp.gsub(/\.xml/, '')
+
+      # open druid-named files
+      doc = Nokogiri::XML(open(file.to_s))
+      record = doc.root
+
+      # add xmlDoc wrapper
+      newFile.write("<xmlDoc id=\"descMetadata\" objectId=\"#{druid}\">" + record.to_xml + '</xmlDoc>')
+
+    elsif file =~ /druid.[a-z]{2}[0-9]{3}[a-z]{2}[0-9]{4}/
+      # get druid from the filename
+      druid = file.gsub(/druid./, '')
+
+      # open druid-named files
+      doc = Nokogiri::XML(open(file.to_s))
+      record = doc.root
+
+      # add xmlDoc wrapper
+      newFile.write("<xmlDoc id=\"descMetadata\" objectId=\"#{druid}\">" + record.to_xml + '</xmlDoc>')
+
+    end
+  end
+
+  newFile.write('</xmlDocs>')
+end
